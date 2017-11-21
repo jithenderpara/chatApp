829258a7c353f8e17a6f69efef67c3514967bb9b
jithenderpara <parajithender@outlook.com>
2017-11-21 18:54:53 +0530

    updated

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -8,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class MessageComponent implements OnInit {
 
   constructor() { }
-
+  public persons: any = [
+    {
+      "name": "Sacha Griffin", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_1.jpg", "status": "online",
+      "statusmsg": "Super deep status message blah blah", "unreadmessages": "11", "lastlogin": ""
+    },
+    {
+      "name": "Debby Jones", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_2.jpeg", "status": "away",
+      "statusmsg": "New day, fresh start, fresh eadaf", "unreadmessages": "", "lastlogin": "Just now"
+    },
+    {
+      "name": "Sarah White", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_3.jpg", "status": "busy",
+      "statusmsg": "Life becomes more peaceful when", "unreadmessages": "", "lastlogin": "2 min"
+    },
+    {
+      "name": "Johhny McGrump", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_4.jpeg", "status": "",
+      "statusmsg": "On vacation for two weeks", "unreadmessages": "", "lastlogin": "Tuesday"
+    },
+    {
+      "name": "Tommy Tom", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_5.jpg", "status": "",
+      "statusmsg": "On vacation for two weeks", "unreadmessages": "", "lastlogin": "Tuesday"
+    },
+    {
+      "name": "James King", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_6.jpg", "status": "",
+      "statusmsg": "Super deep status message afad", "unreadmessages": "", "lastlogin": "Friday"
+    },
+    {
+      "name": "Natsumi Miyu", "pic": "http://vzkiss.com/demo/chatbox/images/avatar/avatar_7.jpg", "status": "online",
+      "statusmsg": "Super deep status message afad", "unreadmessages": "", "lastlogin": "Wednesday"
+    }
+  ];
   ngOnInit() {
   }
 
